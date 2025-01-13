08b3b5a79d6644f6d25de77614d8f2a25e21a01f
Fernanda <fvalentina.gf@gmail.com>
2025-01-12 23:28:42 -0300

    funcion js

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,24 @@
-precio = 400000
+const precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+
+const cantidadTotal = document.querySelector(".cantidad");
+const total = document.querySelector(".valor-total");
+const botonMas = document.querySelector(".mas");
+const botonMenos = document.querySelector(".menos");
+
+let cantidad = 0;
+function valores(){
+    cantidadTotal.textContent = cantidad;
+    total.textContent = `${cantidad*precio}`
+}
+botonMas.onclick = function () {
+    cantidad++;
+    valores();
+}
+botonMenos.onclick = function () {
+    cantidad--;
+    valores();
+}
